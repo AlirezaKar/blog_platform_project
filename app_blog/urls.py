44a44ef2ee3ebb745222ed661e79dd7a92a8150c
AlirezaKar <alireza.k.python1384@gmail.com>
2025-08-10 17:24:42 +0330

    Add post_view and post_view_detail (view + url) + front for all of theses

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import post_detail_view, post_view
+
 urlpatterns = [
-    
+    path('', view=post_view, name='post_view'),
+    path('<int:id>', view=post_detail_view, name='post_detail_view'),    
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import 
+from .models import Post, PostImages, Category, SubCategory, Comment
+
+def post_view(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request=request, template_name='blog/post_view.html', context=context)
+
+def post_detail_view(request, id):
+    try:
+        post = Post.objects.get(id=id)
+        context = {'post':post}
+        return render(request=request, template_name='blog/post_detail_view.html', context=context)
+    except Post.DoesNotExist:
+        return render(request=request, template_name='blog/post_view.html', context={'error':'Object not found.'})
